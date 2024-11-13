5cb423edb8abff6d3cc004e9fa35eb86610becd5
Purna <purnalatha5319@gmail.com>
2024-11-13 10:12:24 +0530

    First commit to maven java project

---
+++ src/main/java/SEGroup/Batch2Java/App.java
@@ -0,0 +1,13 @@
+package SEGroup.Batch2Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
